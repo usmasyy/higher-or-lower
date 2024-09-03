c393eb8f685bdfc38a4c9ff60e8675e2570fc772
usmasyy <usamahsnnn@gmail.com>
2024-09-03 23:56:46 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Card from './Components/Card';
+
+function App() {
+
+  return (
+    <main className="App">
+     <Card />
+    </main>
+  );
+}
+
+export default App;
